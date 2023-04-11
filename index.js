2a9daccd44d7e5187e1a8452013c32a566e9b2d2
Guled <guletkadar@gmail.com>
2023-04-11 11:08:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize (string){
+     return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+      return string.replace(/[^a-zA-Z0-9-' ]+/g, "")
+  }
+  
+  static titleize(string){
+    const exeptWords = ["the","a","an","but","of","and","for","at","by","from"]
+    return string.split(" ").map((word,ind)=>{
+      if(ind === 0 || !exeptWords.includes(word)){
+        return word.charAt(0).toUpperCase() +word.slice(1);
+      }else {
+        return word;
+      }
+    }).join(" ");
+
+  }
+
 }
